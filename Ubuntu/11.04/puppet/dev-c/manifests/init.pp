018250e93f06c9e556e95e9cc7ac3b1158a088ca
Mike Doyle <misterDoyle@gmail.com>
2012-03-07 17:38:42 -0800

    Added module for C development

---
+++ Ubuntu/11.04/puppet/dev-c/manifests/init.pp
@@ -0,0 +1,7 @@
+package { 'gcc': ensure => present, }
+package { 'cmake': ensure => present, }
+package { 'splint': ensure => present, }
+package { 'm4': ensure => present, }
+package { 'automake': ensure => present, }
+package { 'gdb': ensure => present, }
+
